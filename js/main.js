d5dcefa9a4f040da7b1797fa3812ddd9a504bb45
Andrey Sorokin <73.anir@gmail.com>
2021-08-24 22:33:40 +0400

    js slider

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('.slides');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+i = 0;
+
+function activeSlide(n){
+	for(slide of slides)
+	slide.classList.remove('active');
+	slides[n].classList.add('active');
+}
+
+next.addEventListener('click', function(){
+	if(i == slides.length - 1){
+		i = 0;
+		activeSlide(i);
+	} else {
+		i++;
+		activeSlide(i);
+	}
+})
+
+prev.addEventListener('click', function(){
+	if(i == 0){
+		i = slides.length - 1;
+		activeSlide(i);
+	} else {
+		i--;
+		activeSlide(i);
+	}
+})
