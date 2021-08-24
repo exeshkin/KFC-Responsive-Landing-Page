5eff18df3331a5735527a50e8fac5f7c835a4453
Andrey Sorokin <73.anir@gmail.com>
2021-08-25 01:48:14 +0400

    Finish project

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
+/* Toggle menu */
+const toggleMenu = document.querySelector('.toggle');
+const nav = document.querySelector('.nav');
+toggleMenu.onclick = function () {
+	toggleMenu.classList.toggle('active');
+	nav.classList.toggle('active');
+}
+
+
+/* Slider */
 const slides = document.querySelectorAll('.slides');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
 i = 0;
 
-function activeSlide(n){
-	for(slide of slides)
-	slide.classList.remove('active');
+function activeSlide(n) {
+	for (slide of slides)
+		slide.classList.remove('active');
 	slides[n].classList.add('active');
 }
 
-next.addEventListener('click', function(){
-	if(i == slides.length - 1){
+next.addEventListener('click', function () {
+	if (i == slides.length - 1) {
 		i = 0;
 		activeSlide(i);
 	} else {
@@ -20,8 +30,8 @@ next.addEventListener('click', function(){
 	}
 })
 
-prev.addEventListener('click', function(){
-	if(i == 0){
+prev.addEventListener('click', function () {
+	if (i == 0) {
 		i = slides.length - 1;
 		activeSlide(i);
 	} else {
